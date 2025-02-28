2b7969df02731b345d43611ee90c230d7832d53b
GGautamKumar <167999668+GGautamKumar@users.noreply.github.com>
2025-03-01 00:42:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,21 @@ const router=require('./Routes/user.js')
 const app=express();
 app.use(bodyParser.json());
 
-app.get('/',cors(),(req,res)=>{
+app.use(cors(
+    {
+        origin:'http://localhost:5173',
+        credentials:true,
+        methods:['PUT','POST','GET','DELETE'],
+        maxAge:3600
+    }
+));
+
+
+app.get('/',(req,res)=>{
     res.send("Welcome here");
 })
 
-app.use('/api',cors(),router);
+app.use('/api',router);
 
 
 const PORT=process.env.PORT;
