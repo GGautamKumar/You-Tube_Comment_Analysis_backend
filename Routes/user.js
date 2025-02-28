7acc1fa39ddc9e597c50534e24c5036ef3d78f7e
GGautamKumar <167999668+GGautamKumar@users.noreply.github.com>
2025-02-28 23:52:28 +0530

    Update user.js

--- Routes/user.js
+++ Routes/user.js
@@ -1,12 +1,13 @@
 const express=require('express');
+const cors=require('cors');
 
 const {Add}=require('../Controllers/user.js')
 
 const router=express.Router();
 
 
-router.post('/add',Add);
-router.get('/add',(req,res)=>{
+router.post('/add',cors(),Add);
+router.get('/add',cors(),(req,res)=>{
     res.send("welcome to add url");
 })
 
